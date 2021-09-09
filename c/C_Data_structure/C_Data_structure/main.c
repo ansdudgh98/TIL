ddf743d886dd3719a13712a49497f71d6837c90c
youngho Moon <ansdudgh98@naver.com>
2021-09-09 23:58:27 +0900

    fastcampus java example

---
+++ c/C_Data_structure/C_Data_structure/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C_Data_structure
+//
+//  Created by 문영호 on 2021/09/06.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
