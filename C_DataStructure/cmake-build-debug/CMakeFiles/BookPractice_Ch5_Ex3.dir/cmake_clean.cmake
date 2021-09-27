e7e61c70df8956efd37897d68049b27f0ca00286
Moon Young Ho <ansdudgh98@naver.com>
2021-09-27 15:51:44 +0900

    자료구조 큐

---
+++ C_DataStructure/cmake-build-debug/CMakeFiles/BookPractice_Ch5_Ex3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BookPractice_Ch5_Ex3"
+  "BookPractice_Ch5_Ex3.pdb"
+  "CMakeFiles/BookPractice_Ch5_Ex3.dir/BookPractice/Ch5/Ex3.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BookPractice_Ch5_Ex3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C_DataStructure/cmake-build-debug/CMakeFiles/BookPractice_Ch5_Ex4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BookPractice_Ch5_Ex4"
+  "BookPractice_Ch5_Ex4.pdb"
+  "CMakeFiles/BookPractice_Ch5_Ex4.dir/BookPractice/Ch5/Ex4.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BookPractice_Ch5_Ex4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
