8486f98e595453fc5800999694f71cccd4830ef8
문영호 <ansdudgh98@naver.com>
2022-03-17 15:29:31 +0900

    잔디심기 오류 수정 테스트

--- JPA/spring data jpa/data_jpa/src/main/java/study/datajpa/entity/JpaBaseEntity.java
+++ JPA/spring data jpa/data_jpa/src/main/java/study/datajpa/entity/JpaBaseEntity.java
@@ -1,10 +1,15 @@
 package study.datajpa.entity;
 
+import lombok.Getter;
+
 import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
 
-
+@MappedSuperclass
+@Getter
 public class JpaBaseEntity {
 
     @Column(updatable = false)
@@ -15,8 +20,14 @@ public class JpaBaseEntity {
     public void prePersist(){
         LocalDateTime now = LocalDateTime.now();
         createdDate = now;
-        updatedDate = now;
+        updatedDate = null;
     }
+
+    @PreUpdate
+    public void preUpdate(){
+        updatedDate = LocalDateTime.now();
+    }
+
 
 
 }
--- JPA/spring data jpa/data_jpa/src/test/java/study/datajpa/entity/MemberTest.java
+++ JPA/spring data jpa/data_jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -1,9 +1,12 @@
 package study.datajpa.entity;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
@@ -15,6 +18,9 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void testEntity(){
@@ -45,5 +51,29 @@ class MemberTest {
 
 
     }
+
+    @Test
+    public void JpaEventBaseEntity() throws Exception{
+        //given
+        Member member = new Member("member1");
+        memberRepository.save(member);//@PrePersist
+
+
+        Thread.sleep(100);
+        member.setUsername("member2");
+
+        em.flush(); //@preUpdate
+        em.clear();
+        //when
+        Member findMember = memberRepository.findById(member.getId()).get();
+        //then
+        System.out.println("findMember = " + findMember.getCreatedDate());
+        System.out.println("findMember = " + findMember.getLastmodifiedDate());
+
+
+
+    }
+
+
 
 }
