92fb98001d78aab79c1f9d6f5e5f3298a797e718
ansdudgh98 <ansdudgh98@naver.com>
2022-12-04 16:34:30 +0900

    [TIL]12월 4일 - 보라돌이책 소스코드

--- Spring/스프링 부트와 AWS로 혼자 구현하는 웹서비스/book/src/main/java/com/jojoldu/book/springboot/Application.java
+++ Spring/스프링 부트와 AWS로 혼자 구현하는 웹서비스/book/src/main/java/com/jojoldu/book/springboot/Application.java
@@ -1,7 +1,8 @@
-package com.jojoldu.book;
+package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class Application {
--- Spring/스프링 부트와 AWS로 혼자 구현하는 웹서비스/book/src/test/java/com/jojoldu/book/ApplicationTests.java
+++ Spring/스프링 부트와 AWS로 혼자 구현하는 웹서비스/book/src/test/java/com/jojoldu/book/ApplicationTests.java
@@ -1,13 +1,19 @@
 package com.jojoldu.book;
 
+import com.jojoldu.book.springboot.web.HelloController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@WebMvcTest(controllers = HelloController.class)
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
+
 
 }
